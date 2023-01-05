283389385e73536bb3928e75952d036bf874d1ff
Din Mušović <93013218+DinMSC@users.noreply.github.com>
2023-01-05 17:48:13 +0100

    update to form

--- src/pages/customer/CustomerRegister/CustomerRegister.tsx
+++ src/pages/customer/CustomerRegister/CustomerRegister.tsx
@@ -1,25 +1,72 @@
 import { RegContainer } from './CustRegStyles';
 import { TextField, Button } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 const CustomerRegister = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        surname: '',
+        email: '',
+        password1: '',
+        password2: '',
+    });
+
+    const { name, surname, email, password1, password2 } = formData;
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.value]: e.target.name,
+        }));
+    };
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    };
+
     return (
         <RegContainer>
-            <TextField id='standard-basic' label='Name' variant='standard' />
-            <TextField id='standard-basic' label='Surname' variant='standard' />
-            <TextField id='standard-basic' label='Email' variant='standard' />
-            <TextField
-                id='standard-basic'
-                label='Password'
-                variant='standard'
-                type='password'
-            />
-            <TextField
-                id='standard-basic'
-                label='Confirm Password'
-                variant='standard'
-                type='password'
-            />
-            <Button>REGISTER</Button>
+            <form onSubmit={onSubmit}>
+                <input
+                    id='standard-basic'
+                    name='name'
+                    value={name}
+                    placeholder='Name'
+                    onChange={onChange}
+                />
+                <input
+                    id='standard-basic'
+                    name='surname'
+                    value={surname}
+                    placeholder='Surname'
+                    onChange={onChange}
+                />
+                <input
+                    id='standard-basic'
+                    name='email'
+                    value={email}
+                    placeholder='Email'
+                    onChange={onChange}
+                />
+                <input
+                    id='standard-basic'
+                    name='password1'
+                    value={password1}
+                    placeholder='Password'
+                    type='password'
+                    onChange={onChange}
+                />
+                <input
+                    id='standard-basic'
+                    name='password2'
+                    value={password2}
+                    placeholder='Confirm Password'
+                    type='password'
+                    onChange={onChange}
+                />
+
+                <Button>REGISTER</Button>
+            </form>
         </RegContainer>
     );
 };
