858d46115eb3ca014f66e9bc983bd3ca6b1ed7e0
DinMSC <dinmusovic2021@gmail.com>
2023-01-08 16:30:39 +0100

    working form

--- src/pages/customer/CustomerLogin/CustomerLogin.tsx
+++ src/pages/customer/CustomerLogin/CustomerLogin.tsx
@@ -1,8 +1,59 @@
 import { Link } from 'react-router-dom';
+import { RegContainer } from '../CustomerRegister/CustRegStyles';
+import { TextField, Button } from '@mui/material';
+import { useEffect, useState } from 'react';
+import '../CustomerRegister/CustRegStyles/index.css';
 
 const CustomerLogin = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password } = formData;
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(formData);
+    };
     return (
         <div>
+            <RegContainer>
+                <form onSubmit={onSubmit}>
+                    <TextField
+                        sx={{ marginBottom: 1 }}
+                        id='standard-basic'
+                        label='Email'
+                        name='email'
+                        value={email}
+                        placeholder='Email'
+                        onChange={onChange}
+                        type='text'
+                        variant='standard'
+                    />
+                    <TextField
+                        sx={{ marginBottom: 1 }}
+                        id='standard-basic'
+                        label='Password'
+                        name='password'
+                        value={password}
+                        placeholder='Password'
+                        onChange={onChange}
+                        type='password'
+                        variant='standard'
+                    />
+                    <Button variant='contained' type='submit'>
+                        Login
+                    </Button>
+                </form>
+            </RegContainer>
             <div>
                 <Link to={'/customer/register'}>NO ACC REGI HERE</Link>
             </div>
--- src/pages/customer/CustomerRegister/CustomerRegister.tsx
+++ src/pages/customer/CustomerRegister/CustomerRegister.tsx
@@ -1,6 +1,7 @@
 import { RegContainer } from './CustRegStyles';
 import { TextField, Button } from '@mui/material';
 import { useEffect, useState } from 'react';
+import './CustRegStyles/index.css';
 
 const CustomerRegister = () => {
     const [formData, setFormData] = useState({
@@ -16,56 +17,77 @@ const CustomerRegister = () => {
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setFormData((prevState) => ({
             ...prevState,
-            [e.target.value]: e.target.name,
+            [e.target.name]: e.target.value,
         }));
     };
 
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        console.log(formData);
     };
 
     return (
         <RegContainer>
             <form onSubmit={onSubmit}>
-                <input
+                <TextField
+                    sx={{ marginBottom: 1 }}
                     id='standard-basic'
+                    label='Name'
                     name='name'
                     value={name}
                     placeholder='Name'
                     onChange={onChange}
+                    type='text'
+                    variant='standard'
                 />
-                <input
+
+                <TextField
+                    sx={{ marginBottom: 1 }}
                     id='standard-basic'
+                    label='Surname'
                     name='surname'
                     value={surname}
                     placeholder='Surname'
                     onChange={onChange}
+                    type='text'
+                    variant='standard'
                 />
-                <input
+                <TextField
+                    sx={{ marginBottom: 1 }}
                     id='standard-basic'
+                    label='Email'
                     name='email'
                     value={email}
                     placeholder='Email'
                     onChange={onChange}
+                    type='text'
+                    variant='standard'
                 />
-                <input
+                <TextField
+                    sx={{ marginBottom: 1 }}
                     id='standard-basic'
+                    label='Password'
                     name='password1'
                     value={password1}
                     placeholder='Password'
-                    type='password'
                     onChange={onChange}
+                    type='password'
+                    variant='standard'
                 />
-                <input
+                <TextField
+                    sx={{ marginBottom: 1 }}
                     id='standard-basic'
+                    label='Confirm Password'
                     name='password2'
                     value={password2}
                     placeholder='Confirm Password'
-                    type='password'
                     onChange={onChange}
+                    type='password'
+                    variant='standard'
                 />
-
-                <Button>REGISTER</Button>
+                <Button variant='contained' type='submit'>
+                    REGISTER
+                </Button>
             </form>
         </RegContainer>
     );
